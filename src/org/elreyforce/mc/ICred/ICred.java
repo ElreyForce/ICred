a0c99f3f39b18dddc14216b9f55b3575839c34fe
Harald Leithner <eris@elreyforce.org>
2012-03-03 23:24:51 +0100

    Created workspace.

---
+++ src/org/elreyforce/mc/ICred/ICred.java
@@ -0,0 +1,14 @@
+package org.elreyforce.mc.ICred;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ICred extends JavaPlugin {
+	
+	public void onEnable(){ 
+		System.out.println("ICred Version 0.1 enabled.");
+	}
+	 
+	public void onDisable(){ 
+		System.out.println("ICred Version 0.1 disabled.");
+	}		
+}
