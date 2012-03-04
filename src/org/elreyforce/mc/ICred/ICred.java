27a909a2fec828957dad25cc5a59e94af8dc71b3
Harald Leithner <eris@elreyforce.org>
2012-03-04 22:10:05 +0100

    changed from println to logger

--- src/org/elreyforce/mc/ICred/ICred.java
+++ src/org/elreyforce/mc/ICred/ICred.java
@@ -1,14 +1,19 @@
 package org.elreyforce.mc.ICred;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ICred extends JavaPlugin {
 	
-	public void onEnable(){ 
-		System.out.println("ICred Version 0.1 enabled.");
+	Logger log;
+	
+	public void onEnable(){
+		log = this.getLogger();
+		log.info("ICred Version 0.1 enabled.");
 	}
 	 
 	public void onDisable(){ 
-		System.out.println("ICred Version 0.1 disabled.");
+		log.info("ICred Version 0.1 disabled.");
 	}		
 }
